727a2382d5b5330f7db5e4efbb2f35a81eafe207
Alex <alex@MacBook-Air-de-Nunez.local>
2025-07-29 16:13:06 -0600

    Actualizo 2.4

--- db.js
+++ db.js
@@ -3,4 +3,5 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
+console.log('Connecting to DB:', process.env.DATABASE_URL);
 module.exports = pool;
